ecd53393d5da249fe7bf380af7fa6185744c1b6c
Anthony Monte <amonte@princeton.edu>
2024-09-20 18:41:30 -0600

    finish assignment

---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject BulletTemplate;
+    public float shootPower = 1000f;
+    public InputActionReference trigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trigger.action.performed += Shoot;
+    }
+
+    void Shoot(InputAction.CallbackContext __) {
+        GameObject newBullet = Instantiate(BulletTemplate, transform.position, transform.rotation);
+        newBullet.GetComponent<Rigidbody>().AddForce(transform.forward * shootPower);
+    }
+}
